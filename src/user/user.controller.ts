9204af5f4526610d2d0174dc43e94c7cee7df881
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2024-01-17 12:50:20 +0600

    login api added with cookie

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { UserService } from './user.service';
 import { SignUpDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
+import { Response } from 'express';
 
 @Controller('user')
 export class UserController {
@@ -13,5 +15,14 @@ export class UserController {
     signUpDto: SignUpDto,
   ): Promise<{ token: string }> {
     return this.userService.signUp(signUpDto);
+  }
+
+  // Login user
+  @Post('/login')
+  login(
+    @Body() loginDto: LoginDto,
+    @Res({ passthrough: true }) response: Response,
+  ): Promise<{ token: string; user: any }> {
+    return this.userService.login(loginDto, response);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,16 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { SignUpDto } from './dto/signup.dto';
 import { User } from './schemas/user.schema';
 import * as bcrypt from 'bcryptjs';
+import { LoginDto } from './dto/login.dto';
+import { Response } from 'express';
 
 @Injectable()
 export class UserService {
@@ -36,5 +42,37 @@ export class UserService {
 
     const token = this.jwtService.sign({ id: newUser._id });
     return { user: newUser, token };
+  }
+
+  async login(
+    loginDto: LoginDto,
+    response: Response,
+  ): Promise<{ token: string; user: any }> {
+    const { email, password } = loginDto;
+
+    const user = await this.userModel.findOne({ email });
+
+    if (!user) {
+      throw new UnauthorizedException('User not found!');
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      throw new UnauthorizedException('Invalid email or password!');
+    }
+
+    const token = this.jwtService.sign({ id: user._id });
+
+    // Set the cookie in the response
+    response.cookie('refreshToken', token, {
+      httpOnly: true,
+      maxAge: 3600000,
+    }); // Example settings
+
+    const userWithoutPassword = { ...user.toJSON() };
+    delete userWithoutPassword.password;
+
+    return { user: userWithoutPassword, token };
   }
 }
