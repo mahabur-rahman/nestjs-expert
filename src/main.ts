a0362baaee9cbb893f9e14a9ff43dc3b2441ae46
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2024-01-17 20:26:15 +0600

    configure coros with credetials

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as cors from 'cors';
+// import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix(`/api/v1`);
   app.useGlobalPipes(new ValidationPipe());
 
-  app.use(cors());
+  // app.use(cors());
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT || 3000);
 }
