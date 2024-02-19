4c11d2094ca3fa2c4303942acb8f5e8c050bfc48
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2024-02-19 22:04:53 +0600

    delete those file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { UserModule } from './user/user.module';
-import { ResetModule } from './reset/reset.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-    MongooseModule.forRoot(process.env.MONGO_URI),
-    UserModule,
-    ResetModule,
-  ],
   controllers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-// import * as cors from 'cors';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.setGlobalPrefix(`/api/v1`);
-  // Use cookie-parser middleware
-  app.use(cookieParser());
-
-  app.useGlobalPipes(new ValidationPipe());
-
-  // app.use(cors());
-  app.enableCors({
-    origin: ['http://localhost:5173'],
-    credentials: true,
-  });
 
   await app.listen(process.env.PORT || 3000);
 }
