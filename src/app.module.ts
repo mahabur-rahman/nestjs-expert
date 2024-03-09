76e0fd1d47e60c53c9710cf5b0373e9c8f81885f
React | Next | Node <59826852+mahabur-rahman@users.noreply.github.com>
2024-03-09 23:27:05 +0600

    inital state

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,7 @@
-import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { BookModule } from './book/book.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-    MongooseModule.forRoot(process.env.MONGO_URI),
-    BookModule,
-    AuthModule,
-  ],
+  imports: [],
   controllers: [],
 })
 export class AppModule {}
